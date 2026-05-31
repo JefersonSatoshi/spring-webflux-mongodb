83ea77ebddad9a4010a40344f77c77ae125e81c7
jeferson satoshi <jefersonsatoshi@hotmail.com>
2026-05-31 18:45:16 -0300

    Database seeding

--- src/main/java/com/devsuperior/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/devsuperior/workshopmongo/repositories/PostRepository.java
@@ -3,8 +3,8 @@ package com.devsuperior.workshopmongo.repositories;
 import java.time.Instant;
 import java.util.List;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 import com.devsuperior.workshopmongo.entities.Post;
 
