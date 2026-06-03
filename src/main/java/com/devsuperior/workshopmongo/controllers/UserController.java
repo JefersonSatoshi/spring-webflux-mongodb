c1715545ed74ec9fc43f393bbb8445cca57ebf10
jeferson satoshi <jefersonsatoshi@hotmail.com>
2026-06-02 23:43:52 -0300

    DELETE user

--- src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
@@ -1,6 +1,5 @@
 package com.devsuperior.workshopmongo.controllers;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.devsuperior.workshopmongo.dto.PostDTO;
@@ -60,8 +58,7 @@ public class UserController {
 	}
 	
 	@DeleteMapping(value = "/{id}")
-    public ResponseEntity<Void> delete(@PathVariable String id) {
-        service.delete(id);
-        return ResponseEntity.noContent().build();
+    public Mono<ResponseEntity<Void>> delete(@PathVariable String id) {
+        return service.delete(id).then(Mono.just(ResponseEntity.noContent().<Void>build()));
     }
 }
